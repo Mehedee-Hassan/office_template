f8a3242f4f35d430f52f67ee18be8273f1cbd622
Mehedee Hassan <Mehedee-Hassan@users.noreply.github.com>
2020-12-18 15:49:55 +0900

    Create pvalue_dataframe_function.py
    p-value ,funciton

---
+++ ML/correlation/pvalue_dataframe_function.py
@@ -0,0 +1,8 @@
+def calculate_pvalues(df):
+    df = df.dropna()._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            pvalues[r][c] = round(pearsonr(df[r], df[c])[1], 4)
+    return pvalues
