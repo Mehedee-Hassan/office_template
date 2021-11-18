5f66c2d26939074687773111bb6ca6d929428e75
Mehedee Hassan <Mehedee-Hassan@users.noreply.github.com>
2021-11-18 15:23:32 +0900

    injectioncheck

---
+++ php/sql_injectioncheck.php
@@ -0,0 +1,7 @@
+mysql
+Procedural style only: A mysqli object returned by mysqli_connect() or mysqli_init()
+
+string
+The string to be escaped.
+
+Characters encoded are NUL (ASCII 0), \n, \r, \, ', ", and Control-Z.
